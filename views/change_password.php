510dddf66118469a84545760ae886605cf4195b5
osvaldasbekeris-alt <osvaldas.bekeris@stud.svako.lt>
2026-05-17 14:41:32 +0300

    sukurta master slaptazodzio keitimo funkcija

---
+++ views/change_password.php
@@ -0,0 +1,53 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+if (!isset($_SESSION['user_id'])) { header('Location: login.php'); exit; }
+
+require_once '../classes/User.php';
+$klaida = '';
+$sekme = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST['naujas'] != $_POST['pakartoti']) {
+        $klaida = 'Nauji slaptažodžiai nesutampa!';
+    } elseif (strlen($_POST['naujas']) < 6) {
+        $klaida = 'Naujas slaptažodis per trumpas!';
+    } else {
+        $userObj = new User();
+        if ($userObj->changePassword($_SESSION['user_id'], $_POST['senas'], $_POST['naujas'])) {
+            $_SESSION['plain_password'] = $_POST['naujas'];
+            $sekme = 'Slaptažodis sėkmingai pakeistas!';
+        } else {
+            $klaida = 'Senas slaptažodis neteisingas!';
+        }
+    }
+}
+?>
+<!DOCTYPE html><html lang="lt">
+<head>
+    <meta charset="UTF-8">
+    <title>Keisti slaptažodį</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+    <h2>Keisti slaptažodį</h2>
+    <a href="dashboard.php">← Atgal</a><br><br>
+    <?php if ($klaida): ?><p class="klaida"><?= htmlspecialchars($klaida) ?></p><?php endif; ?>
+    <?php if ($sekme): ?><p class="sekme"><?= $sekme ?></p><?php endif; ?>
+    <form method="POST">
+        <label>Senas slaptažodis:</label>
+        <input type="password" name="senas" required>
+        <label>Naujas slaptažodis:</label>
+        <input type="password" name="naujas" required>
+        <label>Pakartoti:</label>
+        <input type="password" name="pakartoti" required>
+        <br><br>
+        <button type="submit">Keisti</button>
+    </form>
+</div>
+</body>
+</html>
