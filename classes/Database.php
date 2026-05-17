a05a96162b96f758cfc9f54d4e549f35ce4225fd
osvaldasbekeris-alt <osvaldas.bekeris@stud.svako.lt>
2026-05-17 14:17:46 +0300

    sukurta Database.php singleton klase duomenims pasiekti

---
+++ classes/Database.php
@@ -0,0 +1,32 @@
+<?php
+class Database {
+    private static $instance = null;
+    private $conn;
+
+    private function __construct() {
+        $host = 'localhost';
+        $db   = 'password_manager';
+        $user = 'root';
+        $pass = '';
+        $charset = 'utf8mb4';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        try {
+             $this->conn = new PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+             throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance == null) {
+            self::$instance = new Database();
+        }
+        return self::$instance->conn;
+    }
+}
